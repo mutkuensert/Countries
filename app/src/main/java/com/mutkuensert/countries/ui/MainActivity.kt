1552392842150ad213007861bc417e7d8cfa49e5
M. Utku Ensert <97624869+mutkuensert@users.noreply.github.com>
2022-11-09 02:11:24 +0300

    Update navigation

--- app/src/main/java/com/mutkuensert/countries/ui/MainActivity.kt
+++ app/src/main/java/com/mutkuensert/countries/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.mutkuensert.countries.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.mutkuensert.countries.R
+import com.mutkuensert.countries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setNavigation()
     }
+
+    private fun setNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    navController.navigate(R.id.homePageFragment)
+                    true
+                }
+
+                R.id.saved -> {
+                    navController.navigate(R.id.savedFragment)
+                    true
+                }
+                else -> { true }
+            }
+        }
+    }
+
 }
